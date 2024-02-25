8bb7099330d9f158180a08a5ea2d13f0f87068bf
atpoverload <atpoverload@gmail.com>
2024-02-25 12:28:20 -0700

    generalizing the forward alignment

--- src/jcarbon/src/main/java/jcarbon/cpu/jiffies/JiffiesAccounting.java
+++ src/jcarbon/src/main/java/jcarbon/cpu/jiffies/JiffiesAccounting.java
@@ -1,78 +1,49 @@
 package jcarbon.cpu.jiffies;
 
+import static jcarbon.data.DataOperations.forwardAlign;
 import static jcarbon.data.DataOperations.forwardApply;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import jcarbon.data.TimeOperations;
 
 public final class JiffiesAccounting {
   public static List<TaskActivityInterval> accountTasks(
       List<ProcessSample> process, List<SystemSample> system) {
-    List<ProcessJiffiesInterval> procInterval =
-        forwardApply(process, ProcessJiffiesInterval::between);
-    List<SystemJiffiesInterval> sysInterval = forwardApply(system, SystemJiffiesInterval::between);
-    return alignJiffies(procInterval.iterator(), sysInterval.iterator());
+    return forwardAlign(
+        forwardApply(process, ProcessJiffiesInterval::between),
+        forwardApply(system, SystemJiffiesInterval::between),
+        JiffiesAccounting::accountInterval);
   }
 
-  private static List<TaskActivityInterval> alignJiffies(
-      Iterator<ProcessJiffiesInterval> procIt, Iterator<SystemJiffiesInterval> sysIt) {
-    ProcessJiffiesInterval proc = procIt.next();
-    SystemJiffiesInterval sys = sysIt.next();
-    ArrayList<TaskActivityInterval> activity = new ArrayList<>();
-    while (true) {
-      // TODO: there needs to be a better way to check if intervals overlap.
-      if (!TimeOperations.lessThan(proc.start(), sys.end())) {
-        if (!procIt.hasNext()) {
-          break;
-        }
-        proc = procIt.next();
-        continue;
-      }
-      if (!TimeOperations.lessThan(sys.start(), proc.end())) {
-        if (!sysIt.hasNext()) {
-          break;
-        }
-        sys = sysIt.next();
-        continue;
-      }
-
-      ArrayList<TaskActivity> tasks = new ArrayList<>();
-      int[] totalJiffies = new int[sys.data().length];
-      for (TaskJiffies reading : proc.data()) {
-        totalJiffies[reading.cpu] += reading.totalJiffies;
-      }
-      for (TaskJiffies reading : proc.data()) {
-        if (reading.totalJiffies == 0) {
-          continue;
-        }
-        double cpuJiffies = sys.data()[reading.cpu].activeJiffies;
-        double taskActivity =
-            Math.min(1.0, reading.totalJiffies / Math.max(cpuJiffies, totalJiffies[reading.cpu]));
-        tasks.add(new TaskActivity(reading.taskId, reading.processId, reading.cpu, taskActivity));
-      }
-      if (tasks.size() > 0) {
-        activity.add(
-            new TaskActivityInterval(
-                TimeOperations.max(proc.start(), sys.start()),
-                TimeOperations.min(proc.end(), sys.end()),
-                tasks));
-      }
-
-      if (TimeOperations.lessThan(proc.start(), sys.start())) {
-        if (!procIt.hasNext()) {
-          break;
-        }
-        proc = procIt.next();
-      } else {
-        if (!sysIt.hasNext()) {
-          break;
-        }
-        sys = sysIt.next();
-      }
+  private static Optional<TaskActivityInterval> accountInterval(
+      ProcessJiffiesInterval proc, SystemJiffiesInterval sys) {
+    ArrayList<TaskActivity> tasks = new ArrayList<>();
+    int[] totalJiffies = new int[sys.data().length];
+    for (TaskJiffies reading : proc.data()) {
+      totalJiffies[reading.cpu] += reading.totalJiffies;
     }
-    return activity;
+    for (TaskJiffies reading : proc.data()) {
+      if (reading.totalJiffies == 0) {
+        continue;
+      }
+      double cpuJiffies = sys.data()[reading.cpu].activeJiffies;
+      double taskActivity =
+          Math.min(
+              1.0,
+              reading.totalJiffies / Math.max(1, Math.max(cpuJiffies, totalJiffies[reading.cpu])));
+      tasks.add(new TaskActivity(reading.taskId, reading.processId, reading.cpu, taskActivity));
+    }
+    if (!tasks.isEmpty()) {
+      return Optional.of(
+          new TaskActivityInterval(
+              TimeOperations.max(proc.start(), sys.start()),
+              TimeOperations.min(proc.end(), sys.end()),
+              tasks));
+    } else {
+      return Optional.empty();
+    }
   }
 
   private JiffiesAccounting() {}
--- src/jcarbon/src/main/java/jcarbon/data/DataOperations.java
+++ src/jcarbon/src/main/java/jcarbon/data/DataOperations.java
@@ -1,17 +1,63 @@
 package jcarbon.data;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
 public final class DataOperations {
   /** Applys a method between pairs of adjacent values in ascending order. */
-  public static <T, U> List<U> forwardApply(List<T> data, BiFunction<T, T, U> difference) {
+  public static <T, U> List<U> forwardApply(List<T> data, BiFunction<T, T, U> func) {
     ArrayList<U> diffs = new ArrayList<>();
     for (int i = 0; i < data.size() - 1; i++) {
-      diffs.add(difference.apply(data.get(i), data.get(i + 1)));
+      diffs.add(func.apply(data.get(i), data.get(i + 1)));
     }
     return diffs;
+  }
+
+  /** Applys a method between pairs of intervals along their time axis. */
+  public static <T extends Interval<?>, U extends Interval<?>, V> List<V> forwardAlign(
+      List<T> firstData, List<U> secondData, BiFunction<T, U, Optional<V>> func) {
+    Iterator<T> firstIt = firstData.iterator();
+    T first = firstIt.next();
+
+    Iterator<U> secondIt = secondData.iterator();
+    U second = secondIt.next();
+
+    ArrayList<V> alignedData = new ArrayList<>();
+    while (true) {
+      // TODO: there needs to be a better way to check if intervals overlap.
+      if (!TimeOperations.lessThan(first.start(), second.end())) {
+        if (!firstIt.hasNext()) {
+          break;
+        }
+        first = firstIt.next();
+        continue;
+      }
+      if (!TimeOperations.lessThan(second.start(), first.end())) {
+        if (!secondIt.hasNext()) {
+          break;
+        }
+        second = secondIt.next();
+        continue;
+      }
+
+      func.apply(first, second).ifPresent(alignedData::add);
+
+      if (TimeOperations.lessThan(first.start(), second.start())) {
+        if (!firstIt.hasNext()) {
+          break;
+        }
+        first = firstIt.next();
+      } else {
+        if (!secondIt.hasNext()) {
+          break;
+        }
+        second = secondIt.next();
+      }
+    }
+    return alignedData;
   }
 
   private DataOperations() {}
