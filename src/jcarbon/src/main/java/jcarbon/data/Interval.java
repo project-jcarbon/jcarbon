db4f8720c61616d35ecea5b44f7e17d1a36be91f
atpoverload <atpoverload@gmail.com>
2024-02-25 17:25:15 -0700

    removing some junk

--- src/jcarbon/src/main/java/jcarbon/data/Interval.java
+++ src/jcarbon/src/main/java/jcarbon/data/Interval.java
@@ -3,7 +3,6 @@ package jcarbon.data;
 import java.time.Instant;
 
 /** A piece of data over a timestamped range. */
-// TODO: might need to make this abstract to 
 public interface Interval<T> {
   Instant start();
 
