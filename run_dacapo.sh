e6d3195531aa7264b70648abd9c3cbee44e75c68
atpoverload <atpoverload@gmail.com>
2024-03-26 22:15:34 -0600

    adding benchmark driver scripts

---
+++ run_dacapo.sh
@@ -0,0 +1,70 @@
+DATA_DIR=data
+mkdir -p "${DATA_DIR}"
+
+ITERATIONS=20
+LOCALE=USA
+
+run_benchmark() {
+    local data_dir="${DATA_DIR}/${BENCHMARK}"
+    mkdir -p "${data_dir}"
+    java \
+        -Djcarbon.benchmarks.output="${data_dir}" \
+        -Djcarbon.emissions.locale="${LOCALE}" \
+        -jar bazel-bin/benchmarks/dacapo_deploy.jar \
+        -n ${ITERATIONS} --no-validation \
+        -c jcarbon.benchmarks.JCarbonCallback \
+        ${BENCHMARK} -s ${SIZE}
+}
+
+# default size dacapo benchmarks
+BENCHMARKS=(
+    biojava
+    cassandra
+    fop
+    h2o
+    jme
+    jython
+    kafka
+    luindex
+    lusearch
+    tradebeans
+    tradesoap
+    xalan
+    zxing
+)
+
+SIZE=default
+
+for BENCHMARK in ${BENCHMARKS[@]}; do
+    run_benchmark
+done
+
+# large size dacapo benchmarks
+BENCHMARKS=(
+    avrora
+    batik
+    eclipse
+    graphchi
+    h2
+    pmd
+    sunflow
+    tomcat
+)
+
+SIZE=large
+
+for BENCHMARK in ${BENCHMARKS[@]}; do
+    run_benchmark
+done
+
+# huge size dacapo benchmarks
+# TODO: need to update and setup the new dacapo with the big data
+# BENCHMARKS=(
+#     graphchi
+# )
+
+# SIZE=huge
+
+# for BENCHMARK in ${BENCHMARKS[@]}; do
+#     run_benchmark
+# done
---
+++ run_renaissance.sh
@@ -0,0 +1,49 @@
+DATA_DIR=data
+mkdir -p "${DATA_DIR}"
+
+ITERATIONS=20
+LOCALE=USA
+
+run_benchmark() {
+    local data_dir="${DATA_DIR}/${BENCHMARK}"
+    mkdir -p "${data_dir}"
+    java \
+        -Djcarbon.benchmarks.output="${data_dir}" \
+        -Djcarbon.emissions.locale="${LOCALE}" \
+        -jar bazel-bin/benchmarks/dacapo_deploy.jar \
+        -r ${ITERATIONS} \
+        --plugin "!jcarbon.benchmark.JCarbonPlugin" \
+        ${BENCHMARK}
+}
+
+BENCHMARKS=(
+    scrabble
+    page-rank
+    future-genetic
+    akka-uct
+    movie-lens
+    scala-doku
+    chi-square
+    fj-kmeans
+    rx-scrabble
+    db-shootout
+    neo4j-analytics
+    finagle-http
+    reactors
+    dec-tree
+    scala-stm-bench7
+    naive-bayes
+    als
+    par-mnemonics
+    scala-kmeans
+    philosophers
+    log-regression
+    gauss-mix
+    mnemonics
+    dotty
+    finagle-chirper
+)
+
+for BENCHMARK in ${BENCHMARKS[@]}; do
+    run_benchmark
+done
