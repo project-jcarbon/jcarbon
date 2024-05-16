cf8e3c9a45e7a9dc078bcf00f32d05d20fe46b56
jlee5205 <jlee5205@gmail.com>
2024-05-16 02:31:53 -0400

    new callback and requirements for glue experiments

--- service/src/main/python/jcarbon/tensorflow/callbacks.py
+++ service/src/main/python/jcarbon/tensorflow/callbacks.py
@@ -1,4 +1,6 @@
 import os
+import time
+import pandas as pd
 
 from tensorflow.keras.callbacks import Callback
 
@@ -55,3 +57,41 @@ class JCarbonBatchCallback(Callback):
             for (signal, component, unit), df in self.reports[-1].groupby(['signal', 'component', 'unit']):
                 # TODO: this should really not ignore negatives
                 logs[f'jcarbon-epoch-{signal}-{component}-{unit}'] = df[df > 0].sum()
+
+
+class JCarbonChunkingCallback(Callback):
+    def __init__(self, addr='localhost:8980', period_ms=DEFAULT_PERIOD_MS, signals=DEFAULT_SIGNALS):
+        self.pid = os.getpid()
+        self.period_ms = period_ms
+        self.client = JCarbonClient(addr)
+        self.signals = signals
+        self.reports = []
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.time = time.time()
+        self.chunks = []
+        self.client.start(self.pid, self.period_ms)
+
+    def on_train_batch_end(self, epoch, logs=None):
+        curr = time.time()
+        if(curr - self.time > 10):
+            self.client.stop(self.pid)
+            self.chunks.append(to_dataframe(
+                self.client.read(self.pid, self.signals)))
+            self.client.start(self.pid, self.period_ms)
+            self.time = curr
+            if logs is not None:
+                for (signal, component, unit), df in self.chunks[-1].groupby(['signal', 'component', 'unit']):
+                    # TODO: this should really not ignore negatives
+                    logs[f'jcarbon-epoch-{signal}-{component}-{unit}'] = df[df > 0].sum()
+                
+    def on_epoch_end(self, epoch, logs=None):
+        self.client.stop(self.pid)
+        self.chunks.append(to_dataframe(
+            self.client.read(self.pid, self.signals)))
+        self.reports.append(pd.concat(self.chunks))
+        if logs is not None:
+            for (signal, component, unit), df in self.reports[-1].groupby(['signal', 'component', 'unit']):
+                # TODO: this should really not ignore negatives
+                logs[f'jcarbon-epoch-{signal}-{component}-{unit}'] = df[df > 0].sum()
+    
