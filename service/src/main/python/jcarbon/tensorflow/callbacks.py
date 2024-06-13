eeca32cdd3665920642327a00e7d4a47e42d3690
atpoverload <atpoverload@gmail.com>
2024-06-13 16:36:03 -0600

    a number of minor changes based on vincent's tests

--- service/src/main/python/jcarbon/tensorflow/callbacks.py
+++ service/src/main/python/jcarbon/tensorflow/callbacks.py
@@ -8,6 +8,7 @@ from jcarbon.client import JCarbonClient
 from jcarbon.report import to_dataframe
 
 DEFAULT_PERIOD_MS = 10
+DEFAULT_PERIOD_SECS = 2
 DEFAULT_SIGNALS = [
     'jcarbon.cpu.eflect.ProcessEnergy',
     'jcarbon.emissions.Emissions',
@@ -60,9 +61,15 @@ class JCarbonBatchCallback(Callback):
 
 
 class JCarbonChunkingCallback(Callback):
-    def __init__(self, addr='localhost:8980', period_ms=DEFAULT_PERIOD_MS, signals=DEFAULT_SIGNALS):
+    def __init__(
+            self,
+            addr='localhost:8980',
+            period_ms=DEFAULT_PERIOD_MS,
+            chunking_period_sec=DEFAULT_PERIOD_SECS,
+            signals=DEFAULT_SIGNALS):
         self.pid = os.getpid()
         self.period_ms = period_ms
+        self.chunking_period_sec = chunking_period_sec
         self.client = JCarbonClient(addr)
         self.signals = signals
         self.reports = []
@@ -74,7 +81,7 @@ class JCarbonChunkingCallback(Callback):
 
     def on_train_batch_end(self, epoch, logs=None):
         curr = time.time()
-        if(curr - self.time > 10):
+        if (curr - self.time > self.chunking_period_sec):
             self.client.stop(self.pid)
             self.chunks.append(to_dataframe(
                 self.client.read(self.pid, self.signals)))
@@ -84,7 +91,7 @@ class JCarbonChunkingCallback(Callback):
                 for (signal, component, unit), df in self.chunks[-1].groupby(['signal', 'component', 'unit']):
                     # TODO: this should really not ignore negatives
                     logs[f'jcarbon-epoch-{signal}-{component}-{unit}'] = df[df > 0].sum()
-                
+
     def on_epoch_end(self, epoch, logs=None):
         self.client.stop(self.pid)
         self.chunks.append(to_dataframe(
@@ -94,4 +101,3 @@ class JCarbonChunkingCallback(Callback):
             for (signal, component, unit), df in self.reports[-1].groupby(['signal', 'component', 'unit']):
                 # TODO: this should really not ignore negatives
                 logs[f'jcarbon-epoch-{signal}-{component}-{unit}'] = df[df > 0].sum()
-    
