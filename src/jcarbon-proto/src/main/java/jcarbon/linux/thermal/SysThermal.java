2a0bda6ae1049b72af9b44976c863c842a4f4398
jlee5205 <jlee5205@gmail.com>
2025-03-12 16:22:22 -0400

    added comments to methods

--- src/jcarbon-proto/src/main/java/jcarbon/linux/thermal/SysThermal.java
+++ src/jcarbon-proto/src/main/java/jcarbon/linux/thermal/SysThermal.java
@@ -33,71 +33,8 @@ public final class SysThermal {
     private static final int ZONE_COUNT = getThermalZoneCount();
     public static final Map<Integer, String> ZONES = getZones();
 
-    private static int getThermalZoneCount() {
-      if (!Files.exists(SYS_THERMAL)) {
-        logger.warning("couldn't check the thermal zone count; thermal sysfs likely not available");
-        return 0;
-      }
-      try {
-        return (int)
-          Files.list(SYS_THERMAL)
-              .filter(p -> p.getFileName().toString().contains("thermal_zone"))
-              .count();
-      } catch (Exception e) {
-        logger.warning("couldn't check the thermal zone count; thermal sysfs likely not available");
-        return 0;
-      }
-    }
-
-    private static Map<Integer, String> getZones(){
-      if (!Files.exists(SYS_THERMAL)) {
-        logger.warning("couldn't check the thermal zones; thermal sysfs likely not available");
-        return Map.of();
-      }
-      try{
-          return
-          Files.list(SYS_THERMAL)
-            .filter(p -> p.getFileName().toString().contains("thermal_zone"))
-            .collect(Collectors.toMap(
-              p -> Integer.parseInt(p.toString().replaceAll("\\D+", "")),
-              p -> {
-                  try{
-                    return Files.readString(p.resolve("type")).toString().trim();
-                  } catch(IOException e){
-                    logger.warning("couldn't read from /sys/class/thermal; thermal sysfs likely not available");
-                    return "";
-                  }
-              } 
-            ));
-      } catch (Exception e) {
-        logger.warning("couldn't check the socket count; thermal sysfs likely not available");
-        return Map.of();
-      }
-    }
-
     public static int getTemperature(int zone){
       return readCounter(zone, "temp");
-    }
-
-    private static int readCounter(int cpu, String component) {
-      String counter = readFromComponent(cpu, component);
-      if (counter.isBlank()) {
-        return 0;
-      }
-      return Integer.parseInt(counter);
-    }
-
-    private static synchronized String readFromComponent(int cpu, String component) {
-      try {
-        return Files.readString(getComponentPath(cpu, component)).trim();
-      } catch (Exception e) {
-        // e.printStackTrace();
-        return "";
-      }
-    }
-
-    private static Path getComponentPath(int zone, String component) {
-      return Paths.get(SYS_THERMAL.toString(), String.format("thermal_zone%d", zone), component);
     }
 
     public static Optional<ThermalZonesSample> sample() {
@@ -150,6 +87,71 @@ public final class SysThermal {
         }
       }
       return temperatures;
+    }
+
+    /** Reads thermal zone information from /sys/class/thermal/ and returns the number of available thermal zones. */
+    private static int getThermalZoneCount() {
+      if (!Files.exists(SYS_THERMAL)) {
+        logger.warning("couldn't check the thermal zone count; thermal sysfs likely not available");
+        return 0;
+      }
+      try {
+        return (int)
+          Files.list(SYS_THERMAL)
+              .filter(p -> p.getFileName().toString().contains("thermal_zone"))
+              .count();
+      } catch (Exception e) {
+        logger.warning("couldn't check the thermal zone count; thermal sysfs likely not available");
+        return 0;
+      }
+    }
+
+    /** Reads thermal zone information from /sys/class/thermal/ and returns a map of each thermal zone to its type. */
+    private static Map<Integer, String> getZones(){
+      if (!Files.exists(SYS_THERMAL)) {
+        logger.warning("couldn't check the thermal zones; thermal sysfs likely not available");
+        return Map.of();
+      }
+      try{
+          return
+          Files.list(SYS_THERMAL)
+            .filter(p -> p.getFileName().toString().contains("thermal_zone"))
+            .collect(Collectors.toMap(
+              p -> Integer.parseInt(p.toString().replaceAll("\\D+", "")),
+              p -> {
+                  try{
+                    return Files.readString(p.resolve("type")).toString().trim();
+                  } catch(IOException e){
+                    logger.warning("couldn't read from /sys/class/thermal; thermal sysfs likely not available");
+                    return "";
+                  }
+              } 
+            ));
+      } catch (Exception e) {
+        logger.warning("couldn't check the socket count; thermal sysfs likely not available");
+        return Map.of();
+      }
+    }
+
+    private static int readCounter(int cpu, String component) {
+      String counter = readFromComponent(cpu, component);
+      if (counter.isBlank()) {
+        return 0;
+      }
+      return Integer.parseInt(counter);
+    }
+
+    private static synchronized String readFromComponent(int cpu, String component) {
+      try {
+        return Files.readString(getComponentPath(cpu, component)).trim();
+      } catch (Exception e) {
+        // e.printStackTrace();
+        return "";
+      }
+    }
+
+    private static Path getComponentPath(int zone, String component) {
+      return Paths.get(SYS_THERMAL.toString(), String.format("thermal_zone%d", zone), component);
     }
 
     private SysThermal(){}
