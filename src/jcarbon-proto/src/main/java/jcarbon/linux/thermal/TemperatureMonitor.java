928fe09bf65c5b82460d8c7e65732e7a91a6577d
jlee5205 <jlee5205@gmail.com>
2025-03-12 23:10:07 -0400

    refactored the monitor and fixed conversion typo

--- src/jcarbon-proto/src/main/java/jcarbon/linux/thermal/TemperatureMonitor.java
+++ src/jcarbon-proto/src/main/java/jcarbon/linux/thermal/TemperatureMonitor.java
@@ -9,9 +9,19 @@ final class TemperatureMonitor {
   private static final Logger logger = getLogger();
 
   public static void main(String[] args) throws Exception {
-    while (true) {
+    int zone = Integer.parseInt(args[0]);
+    int temperature = Integer.parseInt(args[1]);
+    int sample_temp = 0;
+    long previousTime = System.currentTimeMillis();
+
+    while (sample_temp == 0 || temperature < sample_temp) {
+      sample_temp = SysThermal.getTemperature(0);
+      logger.info(String.format("Temperature for thermal zone %s is at %s", zone, sample_temp));
       Thread.sleep(10);
-      logger.info(String.format("%s", SysThermal.sample().get()));
     }
+    
+    long currentTime = System.currentTimeMillis();
+    double elapsed = (currentTime - previousTime) / 1000.0;
+    logger.info(String.format("Cooling down to %s Celsius took %s seconds", temperature, elapsed));
   }
 }
